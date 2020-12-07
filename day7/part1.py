23ad3cf9ff79472fbf9409ff77b37c8ca00e3091
Erik van Weelderen <erikvanweelderen@hotmail.nl>
2020-12-07 17:12:53 +0100

    Bad version check_bags. Inverted bag_tree

--- day7/part1.py
+++ day7/part1.py
@@ -1,4 +1,36 @@
-from day7.read_input import read_input_file
+from day7.read_input import read_input_file, invert_bag
+
+bag_dict = read_input_file('input.txt')
+
+
+def check_bags(bag, val=0):
+    global bag_dict
+
+    for bag, value in bag.items():
+        # print(bag, value)
+        if isinstance(value, int):
+            if bag == 'shiny gold':
+                val += 1
+                break
+
+            if bag in bag_dict:
+                val = check_bags(bag_dict[bag], val)
+
+        if isinstance(value, dict):
+            val = check_bags(value, val)
+
+    return val
+
 
 if __name__ == '__main__':
-    read_input_file('test.txt')
+    print(bag_dict)
+    inv_bag = invert_bag(bag_dict)
+    print(inv_bag)
+    print(len(inv_bag))
+
+    print(inv_bag['shiny gold'])
+    # bag_dict = read_input_file('test.txt')
+    # for key, value in bag_dict.items():
+    #     print(key, value)
+    # print("")
+    # print(check_bags(bag_dict))
--- day7/read_input.py
+++ day7/read_input.py
@@ -2,12 +2,37 @@ import re
 
 
 def read_input_file(file):
-    with open('input.txt') as f:
-        input_list = [re.split(' bags contain | bags, | bag, ', x.strip(' bags.\n')) for x in f]
+    with open(file) as f:
+        input_list = [re.split(' bags contain | bags, | bag, | bags.| bag.', x.strip('\n')) for x in f]
+        for elem in input_list:
+            elem.remove('')
 
     bag_dict = {}
     for elem in input_list:
-        bag_dict[elem[0]] = elem[1:]
+        sub_dict = {}
+
+        for sub_elem in elem[1:]:
+            if sub_elem == 'no other':
+                sub_dict = {'empty': 0}
+
+            else:
+                value, key = sub_elem.split(' ', 1)
+                sub_dict[key] = eval(value)
+
+        bag_dict[elem[0]] = sub_dict
+
+    return bag_dict
 
 
-    print(bag_dict)
+def invert_bag(bag_dict):
+    inverted_bag_dict = {}
+
+    for bag, value in bag_dict.items():
+        for loop_bag, loop_value in bag_dict.items():
+            if bag in loop_value:
+                if bag in inverted_bag_dict:
+                    inverted_bag_dict[bag].append(loop_bag)
+                else:
+                    inverted_bag_dict[bag] = [loop_bag]
+
+    return inverted_bag_dict
