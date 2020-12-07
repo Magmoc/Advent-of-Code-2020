8dfffbc2eac5a1708127b3fcf4b534a1bf626871
Erik van Weelderen <erikvanweelderen@hotmail.nl>
2020-12-07 20:11:03 +0100

    Dank u SinterReiniertje

--- day7/part2.py
+++ day7/part2.py
@@ -3,17 +3,16 @@ from day7.read_input import read_input_file, invert_bag
 bag_dict = read_input_file('input.txt')
 
 
-def count_bags(sub_dict, count=0):
-    print(sub_dict)
+def count_bags(sub_dict):
+    count = 0
+
     if sub_dict:
         for bag, value in sub_dict.items():
-            if bag in bag_dict:
-                print(count)
-                count += value * count_bags(bag_dict[bag], count)
-                return count
-
+            count += value * (1 + count_bags(bag_dict[bag]))
     else:
-        return 1
+        return 0
+
+    return count
 
 
 if __name__ == '__main__':
