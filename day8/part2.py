c6287ad5c1241a80f519907907d0262969dd9fef
Erik van Weelderen <erikvanweelderen@hotmail.nl>
2020-12-08 16:29:47 +0100

    Finished part 2!

--- day8/part2.py
+++ day8/part2.py
@@ -1,28 +1,38 @@
 from day8.read_input_file import read_input
 
+accumulator = 0
 
-def read_commands(command_list, accumulator=0, indx=0, indx_set=None, indx_set_indx=1):
+
+def iter_jmp_nop(input_list):
+    global accumulator
+
+    for i in range(len(input_list)):
+        accumulator = 0
+        new_input_list = [x[:] for x in input_list]
+
+        if new_input_list[i][0] == 'jmp':
+            new_input_list[i][0] = 'nop'
+        elif new_input_list[i][0] == 'nop':
+            new_input_list[i][0] = 'jmp'
+
+        if read_commands(new_input_list, indx_list=[]):
+            return True
+
+
+def read_commands(command_list, indx=0, indx_list=None):
+    global accumulator
+
+    if indx >= len(command_list):
+        return True
+
     command = command_list[indx]
 
-    if indx in indx_set:
-        last_idx = list(indx_set)[-indx_set_indx]
-
-        print("index, lastindex: ", indx, last_idx)
-
-        old_command = command_list[last_idx]
-        print("Old command: ", old_command)
-
-        if old_command[0] == 'jmp': # if jmp it should nop
-            accumulator = read_commands(command_list, accumulator, last_idx+1, indx_set, indx_set_indx)
-
-        elif old_command[0] == 'nop': # if nop it should jmp
-            accumulator = read_commands(command_list, accumulator, last_idx+eval(old_command[1]), indx_set, indx_set_indx)
-
-        elif old_command[0] == 'acc': # if acc should evaluate previous old command
-            indx_set_indx += 1
-            accumulator = read_commands(command_list, accumulator, last_idx, indx_set, indx_set_indx)
+    if indx in indx_list:
+        return False
 
     else:
+        indx_list.append(indx)
+
         if command[0] == 'acc':
             accumulator += eval(command[1])
 
@@ -32,14 +42,14 @@ def read_commands(command_list, accumulator=0, indx=0, indx_set=None, indx_set_i
         elif command[0] == 'nop':
             pass
 
-        indx_set.add(indx)
         indx += 1
-        accumulator = read_commands(command_list, accumulator, indx, indx_set, indx_set_indx)
+        if read_commands(command_list, indx, indx_list):
+            return True
 
-    return accumulator
 
 if __name__ == '__main__':
     input_list = read_input('input.txt')
-    print(input_list)
-    accumulator = read_commands(input_list, indx_set=set())
-    print(accumulator)
+    if iter_jmp_nop(input_list):
+        print(accumulator)
+
+# IT'S ALIVE!
